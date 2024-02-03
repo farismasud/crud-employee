20f46733d5da3cbe8c1f8f6fe0fd31318ed7c518
farismasud <masudfaris54@gmail.com>
2024-02-03 14:16:29 +0700

    Update & Delete

--- controller/index_employee.go
+++ controller/index_employee.go
@@ -1,13 +1,47 @@
 package controller
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
 	"path/filepath"
 )
 
-func NewIndexEmployee() func(w http.ResponseWriter, r *http.Request) {
+type Employee struct {
+	Id string
+	Name string
+	NPWP string
+	Address string
+}
+
+func NewIndexEmployee(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT id, name, npwp, address FROM employee")
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close()
+
+		var employees []Employee
+		for rows.Next() {
+			var employee Employee
+
+			err = rows.Scan(
+				&employee.Id, 
+				&employee.Name, 
+				&employee.NPWP, 
+				&employee.Address)
+			
+			employees = append(employees, employee)
+		}
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
 		fp := filepath.Join("views", "index.html")
 		tmpl, err := template.ParseFiles(fp)
 		if err != nil {
@@ -16,7 +50,10 @@ func NewIndexEmployee() func(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		err = tmpl.Execute(w, nil)
+		data := make(map[string]any)
+		data["employees"] = employees
+
+		err = tmpl.Execute(w, data)
 		if err != nil {
 			w.Write([]byte(err.Error()))
 			w.WriteHeader(http.StatusInternalServerError)
--- routes/routes.go
+++ routes/routes.go
@@ -1,11 +1,16 @@
 package routes
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/farismasud/crud-employee-go/controller"
 )
 
-func MapRoutes(server *http.ServeMux) {
+func MapRoutes(server *http.ServeMux, db *sql.DB) {
 	server.HandleFunc("/", controller.NewHelloWorldController())
+	server.HandleFunc("/employee", controller.NewIndexEmployee(db))
+	server.HandleFunc("/employee/create", controller.NewCreateEmployeeController(db))
+	server.HandleFunc("/employee/update", controller.NewUpdateEmployeeController(db))
+	server.HandleFunc("/employee/delete", controller.NewDeleteEmployeeController(db))
 }
